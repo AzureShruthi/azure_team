47c455ded00025ea3228fe1357a54e25c2974489
AzureShruthi <azure.shruthi@gmail.com>
2022-01-16 11:41:10 +0530

    This is the demo I am pushing

---
+++ script2.ps1
@@ -0,0 +1,42 @@
+﻿$j = 0
+while($j -lt 20)
+{
+$j++
+}
+
+
+$j=0
+
+$j = 0
+while ($j -lt 20) 
+{
+$j++ 
+}
+
+if ($j -eq 15)
+{
+continue
+}
+write-host $j
+
+$j = 0
+while ($j -lt 20)
+{
+$j++
+}
+if($j -eq 18)
+{
+break
+}
+write-host $j++
+
+$k = 0
+while ($k -lt 20)
+{
+$k++
+if($k -eq 15)
+{
+break
+}
+write-host $k
+}
